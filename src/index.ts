1fc924f9dbd5d524f0e8d1064e0df84625faa5c0
Arthur Oliveira <arthur_feelipe@hotmail.com>
2026-06-13 10:26:21 -0300

    configura pool de conexões e testa acesso ao banco

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import express from "express";
 import type { Response, Request } from "express";
+import pool from "./database/conexao.js";
 
 const app = express();
 
-app.get("/clientes", (req: Request, res: Response) => {
-  res.send("ok");
+app.get("/clientes", async (req: Request, res: Response) => {
+  let conexao;
+
+  try {
+    conexao = await pool.getConnection();
+
+    console.log("Conexão realizada com sucesso");
+    res.status(200).send("Conexão criada com sucesso");
+  } catch (erro) {
+    console.error(erro);
+    res.status(500).send("Erro ao conectar banco");
+  } finally {
+    conexao?.release();
+  }
 });
 
 app.listen(900, () => {
