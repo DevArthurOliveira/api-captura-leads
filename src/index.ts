51fce873dda0a78ccdccd6622485f92151fbfdbd
Arthur Oliveira <arthur_feelipe@hotmail.com>
2026-06-23 20:04:14 -0300

    criar rota GET /leads/:id

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,34 @@ dotenv.config();
 import express from "express";
 import type { Response, Request } from "express";
 import pool from "./database/conexao.js";
-import { retornaClientes } from "./services/clientesServices.js";
+import { retornaLeads, retornaLeadsId } from "./services/clientesServices.js";
 
 const app = express();
 
 app.get("/leads", async (req: Request, res: Response) => {
   try {
-    const clientes = await retornaClientes();
+    const clientes = await retornaLeads();
     res.json(clientes);
   } catch (erro) {
     res.status(500).json("Erro: erro interno do servidor");
   }
 });
 
-app.listen(900, () => {
+app.get("/leads/:id", async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  if (isNaN(id) || id <= 0) {
+    return res.status(400).json({ error: "ID inválido" });
+  }
+
+  try {
+    const lead = await retornaLeadsId(id);
+    res.json(lead);
+  } catch (erro) {
+    res.status(500).json("Erro: erro interno no servidor");
+  }
+});
+
+app.listen(3000, () => {
   console.log("Olá Arthur, servidor iniciado.");
 });
--- src/services/clientesServices.ts
+++ src/services/clientesServices.ts
@@ -1,19 +1,28 @@
 import pool from "../database/conexao.js";
 import { Leads } from "../types/usuario.js";
 
-export async function retornaQuery(query: string, valores: unknown[] = []) {
+export async function retornaQuery<T>(query: string, valores: unknown[] = []) {
   const conexao = await pool.getConnection();
   try {
     const [clientes] = await conexao.query(query, valores);
-    return clientes;
+    return clientes as T;
   } finally {
     conexao.release();
   }
 }
 
-export async function retornaClientes(): Promise<Leads[]> {
+export async function retornaLeads(): Promise<Leads[]> {
   const query = "SELECT * FROM leads";
-  const clientes = await retornaQuery(query);
+  const leads = await retornaQuery<Leads[]>(query);
 
-  return clientes as Leads[];
+  return leads as Leads[];
+}
+
+export async function retornaLeadsId(id: number): Promise<Leads> {
+  const query = "SELECT * FROM leads WHERE id = ?";
+  const valores = id;
+
+  const leads = await retornaQuery<Leads[]>(query, [valores]);
+
+  return leads[0];
 }
