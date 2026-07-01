c57dd6330ef11cd257459d0562131deee85c8da6
Arthur Oliveira <arthur_feelipe@hotmail.com>
2026-07-01 18:49:37 -0300

    melhora validações e tratamento de erros na rota GET /leads/:id

--- src/index.ts
+++ src/index.ts
@@ -21,15 +21,24 @@ app.get("/leads", async (req: Request, res: Response) => {
 app.get("/leads/:id", async (req: Request, res: Response) => {
   const id = Number(req.params.id);
 
-  if (isNaN(id) || id <= 0) {
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "ID deve ser um número" });
+  }
+
+  if (id <= 0) {
     return res.status(400).json({ error: "ID inválido" });
   }
 
   try {
     const lead = await retornaLeadsId(id);
-    res.json(lead);
+
+    if (!lead) {
+      return res.status(404).json({ error: "Lead não encontrado" });
+    }
+    return res.status(200).json(lead);
   } catch (erro) {
-    res.status(500).json("Erro: erro interno no servidor");
+    console.error(erro);
+    return res.status(500).json({ error: "erro interno no servidor" });
   }
 });
 
